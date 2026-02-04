bfdf20ed11e2736e67c32d624a10cd8e3df0ba78
FrancoPanzone <166449295+FrancoPanzone@users.noreply.github.com>
2026-02-04 20:05:43 -0300

    fix/schemaReviews

--- TP Integrador/backend/src/schemas/review.schema.ts
+++ TP Integrador/backend/src/schemas/review.schema.ts
@@ -5,7 +5,7 @@ import { z } from 'zod';
 export const reviewSchema = z.object({
   user_id: z.number().int().positive(),
   product_id: z.number().int().positive(),
-  qualification: z.number().int().min(1).max(5),
+  qualification: z.number().min(1).max(5),
   comment: z.string().min(3),
   date: z.coerce.date().optional(),
 });
